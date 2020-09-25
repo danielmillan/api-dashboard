e90872380e45486bc31293a5a698ea0433ca422e
Daniel F Millan <dani.millan07@gmail.com>
2020-09-25 16:37:31 -0500

    return config

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const path = require("path");
 const morgan = require("morgan");
 
 // App config
-const config = require("./config");
+const config = require("./server/config");
 const { PORT } = config;
 
 // Create a write stream
 var accessLogStream = fs.createWriteStream(
-  path.join(__dirname, "logs/access.log"),
+  path.join(__dirname, "server/logs/access.log"),
   { flags: "a" }
 );
 
@@ -22,7 +22,7 @@ app.use(morgan("combined", { stream: accessLogStream }));
 app.use(require("body-parser").json());
 
 // Start routes
-app.use(require("./routes"));
+app.use(require("./server/routes"));
 
 module.exports = app; // for testing
 
